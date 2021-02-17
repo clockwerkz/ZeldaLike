ae5cb1aeaf58dceabad48dcca9628dbdcc98c195
clockwerkz <clockwerkz@gmail.com>
2021-02-16 22:28:49 -0800

    adds player movement code - basic movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float speed;
+
+    private Rigidbody2D _myRigidBody;
+    private Vector3 _change;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _change = Vector3.zero;
+        _change.x = Input.GetAxisRaw("Horizontal");
+        _change.y = Input.GetAxisRaw("Vertical");
+        if (_change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter()
+    {
+        _myRigidBody.MovePosition(
+            transform.position + _change.normalized * speed * Time.deltaTime
+        );
     }
 }
