f8d3140768ed0ee3fc7bc7712bfeeb16b3852bb5
Carlos <clockwerkz@gmail.com>
2021-02-17 19:45:13 -0800

    Setting up movement animation in progress

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,11 +9,13 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D _myRigidBody;
     private Vector3 _change;
+    private Animator _animator;
 
     // Start is called before the first frame update
     void Start()
     {
         _myRigidBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,9 +24,19 @@ public class PlayerMovement : MonoBehaviour
         _change = Vector3.zero;
         _change.x = Input.GetAxisRaw("Horizontal");
         _change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
         if (_change != Vector3.zero)
         {
+            _animator.SetBool("moving", true);
             MoveCharacter();
+            _animator.SetFloat("moveX", _change.x);
+            _animator.SetFloat("moveY", _change.y);
+        } else {
+            _animator.SetBool("moving", false);
         }
     }
 
